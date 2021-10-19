d9bcc93a516c60529fbe299307e55235ec6d07cc
dedi <dediirwansyah1925@gmail.com>
2021-10-19 14:00:30 +0700

    membuat animasi pilihan ganda benar atau salah

---
+++ src/components/Trivia.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Trivia = () => {
+    return (
+        <div className="trivia">
+            <div className="question">what's the best youtube channel?</div>
+            <div className="answers">
+                <div className="answer">Lama dev</div>
+                <div className="answer wrong">Lama dev</div>
+                <div className="answer">Lama dev</div>
+                <div className="answer">Lama dev</div>
+            </div>
+        </div>
+    )
+}
+
+export default Trivia
