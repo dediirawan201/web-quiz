db4ca073c0bd134d33d5a11fef861a241bcfb1d6
dedi <dediirwansyah1925@gmail.com>
2021-10-20 11:22:50 +0700

    membuat logic jawaban benar dan salah

--- src/components/Trivia.js
+++ src/components/Trivia.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 
 const Trivia = ({
     data,
-    setTimeout,
+    setStop,
     questionNumber,
     setQuestionNumber
 }) => {
@@ -13,9 +13,27 @@ const Trivia = ({
         setQuestion(data[questionNumber - 1]);
     },[data, questionNumber])
 
+    const delay = (duration, callback) => {
+        setTimeout(() => {
+            callback()
+        }, duration);
+    }
+
     const handleClick = (a) => {
         setSelectedAnswer(a);
         setClassName("answer active");
+        delay(3000, () => {
+            setClassName(a.correct ? "answer correct" : "answer wrong")
+        })
+        delay(6000, () => {
+            if(a.correct){
+                setQuestionNumber((prev) => prev + 1);
+                setSelectedAnswer(null)
+            }else{
+                setStop(true)
+            }
+        })
+        
     }
     return (
         <div className="trivia">
